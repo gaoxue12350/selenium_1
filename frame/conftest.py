bdd9e6aae400c09515ed769f704b49cea1073bee
gaoxue <973140551@qq.com>
2020-11-10 18:34:50 +0800

    实现录屏功能

--- frame/conftest.py
+++ frame/conftest.py
@@ -5,8 +5,10 @@ import pytest
 
 @pytest.fixture(scope="module",autouse=True)
 def record_vedio():
+    #使用scrcpy录屏
     command="scrcpy --record tmp.mp4"
     p=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print(p)
     yield
-    os.kill(p.pid, signal.CTRL_C_EVENT)
+    os.kill(p.pid, signal.SIGTERM)   #mac
+    # os.kill(p.pid, signal.CTRL_C_EVENT)  #win
